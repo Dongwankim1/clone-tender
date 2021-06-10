0e01bf371538299f95d3af1c820db996c33df43e
kdw <zkdlwu94@naver.com>
2021-06-10 22:10:08 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import Header from './component/Header/Header';
+import './App.css';
 
 const App = () =>{
     return (
         <div className="App">
             <h1>Let's build</h1>
+            <Header/>
         </div>
     )
 };
